4b1a1cfe504264d08b82f0266ae90a687a498e93
unknown <jojanssen1@gmail.com>
2022-10-01 13:10:48 +0200

    first-committed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, Observable, startWith, switchMap } from 'rxjs';
+
+
+
+export interface Lyric {
+  lyricId: number;
+  words: string;
+  songTitle: string;
+  performerId: number;
+}
+
+export interface Performer {
+  performerId: number;
+  name: string;
+  lyrics: Lyric[];
+  favouritePerformers: FavouritePerformer[];
+}
+
+export interface Player {
+  playerId: number;
+  name: string;
+  starredLyrics: StarredLyric[];
+  favouritePerformers: FavouritePerformer[];
+}
+
+export interface StarredLyric {
+  id: number;
+  playerId: number;
+  lyricId: number;
+  player: Player;
+  lyric: Lyric;
+}
+
+export interface FavouritePerformer {
+  id: number;
+  playerId: number;
+  performerId: number;
+  player: Player;
+  performer: Performer;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'LyricsFront';
+
+export class AppComponent //implements OnInit
+{
+  makeFilter = new FormControl('');
+  performers : Observable<Performer[]>;
+
+  constructor(private client: HttpClient) {
+    console.log('helloo');
+    this.performers = this.makeFilter.valueChanges
+      .pipe(
+        startWith(''),
+        debounceTime(400),
+        switchMap(q => this.client.get<Performer[]>(
+          `https://localhost:5001/lyrics/performers?SearchQuery=${q}`
+          )))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NoopAnimationsModule, FormsModule, ReactiveFormsModule, HttpClientModule,
+    MatAutocompleteModule, MatFormFieldModule, MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
