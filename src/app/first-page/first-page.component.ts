d197e4b17dd3d5a1123526e7f4dba9b3fbf5c3fa
unknown <jojanssen1@gmail.com>
2022-10-10 23:24:01 +0200

    added material icons

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -1,15 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, VERSION, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { debounceTime, Observable, startWith, switchMap } from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatAutocompleteTrigger } from '@angular/material/autocomplete';
+import { ApiService } from '../api.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ReviewLyricsDialogComponent } from '../reviewLyrics-dialog/review-lyrics-dialog/review-lyrics-dialog.component';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+
+export interface DialogLyricData {
+  dLyrics: string;
+  dSongTitle: string;
+}
+
+export interface Lyric {
+  lyricId?: number;
+  words: string;
+  songTitle: string;
+  performerId: number;
+}
+
+export interface Performer {
+  performerId: number;
+  name: string;
+  lyrics?: Lyric[];
+  favouritePerformers?: FavouritePerformer[];
+}
+
+export interface Player {
+  playerId: number;
+  name: string;
+  starredLyrics: StarredLyric[];
+  favouritePerformers: FavouritePerformer[];
+}
+
+export interface StarredLyric {
+  id: number;
+  playerId: number;
+  lyricId: number;
+  player: Player;
+  lyric: Lyric;
+}
+
+export interface FavouritePerformer {
+  id: number;
+  playerId: number;
+  performerId: number;
+  player: Player;
+  performer: Performer;
+}
+
 
 @Component({
   selector: 'app-first-page',
   templateUrl: './first-page.component.html',
   styleUrls: ['./first-page.component.css']
 })
-export class FirstPageComponent implements OnInit {
+export class FirstPageComponent {
 
-  constructor() { }
+  disablePerfomer: boolean = false;
+  disableButton: boolean = true;
+  makeFilter = new FormControl('');
+  performers : Observable<Performer[]>;
+  performerName? : string;
+  iD: number = 0;
+  value = 'Clear me';
 
-  ngOnInit(): void {
+
+  constructor(private client: HttpClient, public apiService: ApiService, public dialog: MatDialog) {
+    this.performers = this.makeFilter.valueChanges
+    .pipe(
+      startWith(''),
+      debounceTime(400),
+      switchMap(q =>
+        this.client.get<Performer[]>(
+        `https://localhost:5001/lyrics/performers?SearchQuery=${q}`
+        )));
+  }
+
+  onSelection(perf: Performer){
+    this.performerName = perf.name;
+    this.makeFilter.disable;
+    this.disableButton = false;
+    console.log(perf.name, perf.performerId);
+    this.iD = perf.performerId;
+  }
+
+  changePerformer(){
+    this.disableButton = true;
+  }
+
+  RemoveCatFilter(){
+
   }
 
 }
